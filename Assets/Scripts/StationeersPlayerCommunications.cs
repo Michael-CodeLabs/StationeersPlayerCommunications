2082a01c05b73177f9b4ee72d17e5bdfde570ec1
ilodev <ilodev@users.noreply.github.com>
2025-03-26 09:59:16 +0100

    Code clean up, starting/booting classes

--- Assets/Scripts/StationeersPlayerCommunications.cs
+++ Assets/Scripts/StationeersPlayerCommunications.cs
@@ -1,21 +1,21 @@
-using System;
-using BrainClock.PlayerComms;
 using HarmonyLib;
 using System.Linq;
-using StationeersMods.Interface;
 using UnityEngine;
-using Assets.Scripts.Networking;
+using StationeersMods.Interface;
+using BrainClock.PlayerComms;
 
-[StationeersMod("StationeersPlayerCommunications","StationeersPlayerCommunications [StationeersMods]","0.2.4657.21547.1")]
+[StationeersMod("StationeersPlayerCommunications", "StationeersPlayerCommunications [StationeersMods]", "0.2.4657.21547.1")]
 public class StationeersPlayerCommunications : ModBehaviour
 {
-    // private ConfigEntry<bool> configBool;
-    
+    /// <summary>
+    /// StationeersMods/BepinEx ModBehaviour to handle the mod initialization.
+    /// </summary>
+    /// <param name="contentHandler">Contains the assets of the mod package</param>
     public override void OnLoaded(ContentHandler contentHandler)
     {
-        UnityEngine.Debug.Log("StationeersPlayerCommunications.OnLoaded()");
+        UnityEngine.Debug.Log("StationeersPlayerCommunications setup");
         
-        //Config example
+        // Configuration setup.
         // configBool = Config.Bind("Input",
         //     "Boolean",
         //     true,
@@ -23,18 +23,25 @@ public class StationeersPlayerCommunications : ModBehaviour
         
         Harmony harmony = new Harmony("StationeersPlayerCommunications");
 
-        VoiceDataManager.HumanVoicePrefab = contentHandler.prefabs.FirstOrDefault(prefab => prefab.name == "HumanVoiceSource");
+        // The InventoryManager patch will spawn our main Manager into the 
+        // game alongside the rest of game managers. This manager is created
+        // after the game has loaded all files/resources and will survive 
+        // between games.
+        Debug.Log("+ Queueing the spawn of managers");
+        var targetPrefab = contentHandler.prefabs.FirstOrDefault(prefab => prefab.name == "PlayerCommunicationsManagerPrefab");
+        InventoryManagerPatch.PlayerCommunicationsManagerPrefab = targetPrefab;
 
-        //InventoryManagerPatch.VoiceTestPrefab = contentHandler.prefabs.FirstOrDefault(prefab => prefab.name == "VoiceTesting");
-        InventoryManagerPatch.PlayerCommunicationsManagerPrefab = contentHandler.prefabs.FirstOrDefault(prefab => prefab.name == "PlayerCommunicationsManagerPrefab");
-
-        PrefabPatch.prefabs = contentHandler.prefabs;
-        harmony.PatchAll();
-        UnityEngine.Debug.Log("StationeersPlayerCommunications Loaded with " + contentHandler.prefabs.Count + " prefab(s)");
-
-        // Adding custom message type
-        Debug.Log("MessageFactoryInjector injecting AudioClipMessage");
+        // Adding custom message types through an injector. The message Class
+        // needs to be added to the factory in two lookup tables, and because
+        // this message type needs to be processed in the client to it also 
+        // needs an additional injection in the Network process() handler.
+        Debug.Log("+ Injecting network messages");
         MessageFactoryInjector.InjectCustomMessageType(typeof(AudioClipMessage));
 
+        // Add the content (Thing) prefabs to the game.
+        PrefabPatch.prefabs = contentHandler.prefabs;
+        harmony.PatchAll();
+
+        UnityEngine.Debug.Log("+ Patching complete, setup finished");
     }
 }
--- Assets/Scripts/patches/InventoryManagerPatches.cs
+++ Assets/Scripts/patches/InventoryManagerPatches.cs
@@ -10,21 +10,11 @@ namespace BrainClock.PlayerComms
     [HarmonyPatch(typeof(Assets.Scripts.Inventory.InventoryManager), "ManagerAwake")]
     public class InventoryManagerPatch
     {
-        public static GameObject VoiceTestPrefab;
         public static GameObject PlayerCommunicationsManagerPrefab;
 
         static void Postfix(Assets.Scripts.Inventory.InventoryManager __instance)
         {
 
-            if (VoiceTestPrefab != null  && Application.platform != RuntimePlatform.WindowsServer)
-            {
-                GameObject.Instantiate(VoiceTestPrefab, Vector3.zero, Quaternion.identity, __instance.transform);
-                Debug.Log("VoiceTestPrefab spawned after ManagerAwake()");
-            }
-            else
-            {
-                Debug.LogError("Failed to load VoiceTestPrefab!");
-            }
 
             if (PlayerCommunicationsManagerPrefab != null && Application.platform != RuntimePlatform.WindowsServer)
             {
