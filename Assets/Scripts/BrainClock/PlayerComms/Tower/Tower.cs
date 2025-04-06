f26b81298bf9231f2244530621db9db56415d5b2
Michael C <123798868+Michael-CodeLabs@users.noreply.github.com>
2025-04-06 08:01:04 +0200

    Towers meshed are now in. There is now a tower scene. Project clean up.

---
+++ Assets/Scripts/BrainClock/PlayerComms/Tower/Tower.cs
@@ -0,0 +1,12 @@
+using Assets.Scripts.Objects;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrainClock.PlayerComms
+{
+    public class Tower : SmallGrid
+    {
+
+    }
+}
