f41234f789013cd952fd39e79b4f5ff17bc01bd6
Michael C <123798868+Michael-CodeLabs@users.noreply.github.com>
2025-04-08 15:37:28 +0200

    Pushing for backup because im about to remove the mesh filters on the tower

--- Assets/Scripts/BrainClock/PlayerComms/Tower/Tower.cs
+++ Assets/Scripts/BrainClock/PlayerComms/Tower/Tower.cs
@@ -1,4 +1,6 @@
 using Assets.Scripts;
+using Assets.Scripts.Objects;
+using Assets.Scripts.Objects.Structures;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +9,62 @@ namespace BrainClock.PlayerComms
 {
     public class Tower : LargeElectrical
     {
+        //Assign Constructions Materials
+        public override void Start()
+        {
+            base.Start();
+            AssignToolExit();
+            AssignEntryTool();
+        }
 
+        private void AssignToolExit()
+        {
+            var ItemAngleGrinder = Prefab.Find<Item>("ItemAngleGrinder");
+            var ItemWireCutters = Prefab.Find<Item>("ItemWireCutters");
+            var ItemScrewdriver = Prefab.Find<Item>("ItemScrewdriver");
+            var ItemCrowbar = Prefab.Find<Item>("ItemCrowbar");
+
+            if (BuildStates == null) return;
+
+            if (BuildStates.Count > 0)
+                BuildStates[0].Tool.ToolExit = ItemAngleGrinder;
+            if (BuildStates.Count > 1)
+                BuildStates[1].Tool.ToolExit = ItemCrowbar;
+            if (BuildStates.Count > 2)
+                BuildStates[2].Tool.ToolExit = ItemScrewdriver;
+            if (BuildStates.Count > 3)
+                BuildStates[3].Tool.ToolExit = ItemWireCutters;
+        }
+
+        private void AssignEntryTool()
+        {
+            var ItemWeldingTorch = Prefab.Find<Item>("ItemWeldingTorch");
+            var ItemArcWelder = Prefab.Find<Item>("ItemArcWelder");
+            var ItemScrewdriver = Prefab.Find<Item>("ItemScrewdriver");
+            var ItemWireCutters = Prefab.Find<Item>("ItemWireCutters");
+            var ItemSteelSheets = Prefab.Find<Item>("ItemSteelSheets");
+            var ItemPlasticSheets = Prefab.Find<Item>("ItemPlasticSheets");
+            var ItemCableCoilHeavy = Prefab.Find<Item>("ItemCableCoilHeavy");
+
+            if (BuildStates == null) return;
+
+            if (BuildStates.Count > 0)
+            {
+                BuildStates[1].Tool.ToolEntry = ItemWeldingTorch ?? ItemArcWelder;
+                BuildStates[1].Tool.ToolEntry2 = ItemSteelSheets;
+            }
+
+            if (BuildStates.Count > 1)
+            {
+                BuildStates[2].Tool.ToolEntry = ItemPlasticSheets;
+                BuildStates[2].Tool.ToolEntry2 = ItemScrewdriver;
+            }
+
+            if (BuildStates.Count > 2)
+            {
+                BuildStates[3].Tool.ToolEntry = ItemWireCutters;
+                BuildStates[3].Tool.ToolEntry2 = ItemCableCoilHeavy;
+            }
+        }
     }
 }
